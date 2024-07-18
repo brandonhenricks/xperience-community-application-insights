bb11a85ddb5bebefb55c44d63be5571de3ba399a
Brandon Henricks <78803523+bluemodus-brandon@users.noreply.github.com>
2024-07-17 23:01:51 -0400

    Enhanced project configuration and dependencies
    - Introduced `.editorconfig` to standardize coding styles across various file types (C#, VB, XML, JS, JSON, PowerShell, shell scripts) including .NET code style settings.
    - Created `Directory.Build.props` for centralizing MSBuild properties, including company info, package metadata, target frameworks, nullable reference types, warning levels, implicit usings, documentation generation, and package lock file usage. Also, added `CSharpFunctionalExtensions` reference for continuous integration builds.
    - Added `Directory.Build.targets` to manage PDBs and XML documentation in output and refine content output files.
    - Implemented `Directory.Packages.props` for centralized NuGet package version management, covering Kentico Xperience, Microsoft.Extensions, Application Insights, CSharpFunctionalExtensions, and SourceLink GitHub packages.
    - Updated `XperienceCommunity.ApplicationInsights.sln` for Visual Studio 2017 and later compatibility, including project configuration and global settings.
    - Configured `nuget.config` to use `nuget.org` as the sole package source and enabled package source mapping.
    - Extended `RequestTelemetry` with `RequestTelemetryExtensions.cs` for telemetry properties related to routed web pages using Kentico's `RoutedWebPage`.
    - Introduced `XperienceApplicationInsightsFilter.cs` for ASP.NET Core, enriching Application Insights telemetry with website channel and routed web page information via Kentico's interfaces.
    - Updated `XperienceCommunity.ApplicationInsights.csproj` to target .NET 6.0, 7.0, and 8.0, enable implicit usings, nullable reference types, and define package metadata with necessary ASP.NET Core, Application Insights, dependency injection, functional programming extensions, Kentico Xperience, and SourceLink for GitHub package references.
    - General overview provided for `packages.lock.json` changes, indicating potential updates in dependency versions, additions/removals of dependencies, transitive dependency changes, checksum updates, and dependency source changes.

---
+++ src/XperienceCommunity.ApplicationInsights/Extensions/RequestTelemetryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Kentico.Content.Web.Mvc;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace XperienceCommunity.ApplicationInsights.Extensions
+{
+    public static class RequestTelemetryExtensions
+    {
+        /// <summary>
+        /// Adds telemetry properties related to a routed web page to the specified <see cref="RequestTelemetry"/>.
+        /// </summary>
+        /// <param name="requestTelemetry">The <see cref="RequestTelemetry"/> to add properties to.</param>
+        /// <param name="routedWebPage">The <see cref="RoutedWebPage"/> containing the information to be added as properties.</param>
+        public static void AddRoutedWebPageTelemetry(this RequestTelemetry? requestTelemetry,
+            RoutedWebPage? routedWebPage)
+        {
+            if (requestTelemetry is null)
+            {
+                return;
+            }
+
+            if (routedWebPage is null)
+            {
+                return;
+            }
+
+            requestTelemetry.Properties.Add(nameof(WebPageDataContext.WebPage.ContentTypeName), routedWebPage.ContentTypeName);
+            requestTelemetry.Properties.Add(nameof(WebPageDataContext.WebPage.ContentTypeID), $"{routedWebPage.ContentTypeID}");
+            requestTelemetry.Properties.Add(nameof(WebPageDataContext.WebPage.WebPageItemGUID), $"{routedWebPage.WebPageItemGUID}");
+            requestTelemetry.Properties.Add(nameof(WebPageDataContext.WebPage.WebPageItemID), $"{routedWebPage.WebPageItemID}");
+            requestTelemetry.Properties.Add(nameof(WebPageDataContext.WebPage.LanguageName), $"{routedWebPage.LanguageID}");
+            requestTelemetry.Properties.Add(nameof(WebPageDataContext.WebPage.LanguageName), $"{routedWebPage.LanguageName}");
+        }
+    }
+}
---
+++ src/XperienceCommunity.ApplicationInsights/XperienceApplicationInsightsFilter.cs
@@ -0,0 +1,43 @@
+﻿using CMS.Websites.Routing;
+using Kentico.Content.Web.Mvc;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using XperienceCommunity.ApplicationInsights.Extensions;
+
+namespace XperienceCommunity.ApplicationInsights
+{
+    public sealed class XperienceApplicationInsightsFilter : IActionFilter
+    {
+        private readonly IWebsiteChannelContext _websiteChannelContext;
+        private readonly IWebPageDataContextRetriever _webPageDataContextRetriever;
+
+        public XperienceApplicationInsightsFilter(IWebsiteChannelContext websiteChannelContext, IWebPageDataContextRetriever webPageDataContextRetriever)
+        {
+            _websiteChannelContext = websiteChannelContext;
+            _webPageDataContextRetriever = webPageDataContextRetriever;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var requestTelemetry = context.HttpContext.Features.Get<RequestTelemetry>();
+
+            if (requestTelemetry is null)
+            {
+                return;
+            }
+
+            requestTelemetry.Properties.Add(nameof(IWebsiteChannelContext.WebsiteChannelID), $"{_websiteChannelContext.WebsiteChannelID}");
+            requestTelemetry.Properties.Add(nameof(IWebsiteChannelContext.WebsiteChannelName), _websiteChannelContext.WebsiteChannelName);
+            requestTelemetry.Properties.Add(nameof(IWebsiteChannelContext.IsPreview), $"{_websiteChannelContext.IsPreview}");
+
+            if (_webPageDataContextRetriever.TryRetrieve(out var page))
+            {
+                requestTelemetry.AddRoutedWebPageTelemetry(page.WebPage);
+            }
+        }
+    }
+}
